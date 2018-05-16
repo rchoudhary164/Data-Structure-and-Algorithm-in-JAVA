9727ffe49f59821f4df9a314ae4239f59713a84b
Rakesh <rakeshkumar.narayan@justdial.com>
2018-05-16 22:08:44 +0530

    removeKey and insertAfter method added in DoublyLinked List

--- Data_Structure_and_Algorithm/src/ds/doublyLinkedList/com/App.java
+++ Data_Structure_and_Algorithm/src/ds/doublyLinkedList/com/App.java
@@ -9,11 +9,11 @@ public class App {
 		nodeA.insertFirst(5);
 		nodeA.insertFirst(8);
 		nodeA.insertFirst(11);
-		nodeA.view();
+		//nodeA.view();
 		System.out.println("---removing node---");
 		nodeA.removeFirst();
 		nodeA.removeFirst();
-		nodeA.view();
+		//nodeA.view();
 		nodeA.removeFirst();
 		nodeA.removeFirst();
 		nodeA.removeFirst();
@@ -23,7 +23,25 @@ public class App {
 		nodeA.insertLast(9);
 		nodeA.insertLast(11);
 		nodeA.insertFirst(21);
-		nodeA.view();
+		//nodeA.view();
+		System.out.println("-------Another Doubly LinkedList nodeB start here");
+		DoublyLinkedList nodeB = new DoublyLinkedList();
+		nodeB.removeLast();
+		nodeB.insertLast(31);
+		nodeB.removeLast();
+		nodeB.view();
+		nodeB.insertLast(41);
+		nodeB.insertLast(51);
+		nodeB.insertLast(61);
+		nodeB.insertFirst(22);
+		int removed = nodeB.removeLast();
+		System.out.println("removed "+removed);
+		nodeB.insertAfter(41, 55);
+		nodeB.addAfter(41, 9);
+		nodeB.removeKey(51);
+		nodeB.removeKey(22);
+		nodeB.removeKey(9);
+		nodeB.view();
 
 	}
 
--- Data_Structure_and_Algorithm/src/ds/doublyLinkedList/com/DoublyLinkedList.java
+++ Data_Structure_and_Algorithm/src/ds/doublyLinkedList/com/DoublyLinkedList.java
@@ -43,12 +43,88 @@ public class DoublyLinkedList {
 		if(!isEmpty()){
 			newNode.previous = last;
 			last.next = newNode;
-			last = newNode;
 		}else{
 			newNode.next = first;
 			first = newNode;
-			last = newNode;
 		}
+		last = newNode;
+	}
+	public int removeLast(){
+		int temp = -1;
+		if(!isEmpty()){
+			if(last.previous ==null){
+				last = null;
+				first = null;
+			}else{
+				temp = last.data;
+				last = last.previous;
+				last.next = null;
+			}
+		}
+		return temp;
+	}
+	public boolean insertAfter(int after,int data){
+		boolean result = false;
+		Node newNode = new Node();
+		newNode.data = data;
+		Node curNode = first;
+		while(curNode !=null){
+			if(curNode.data == after){
+				newNode.next = curNode.next;
+				curNode.next.previous = newNode;
+				newNode.previous = curNode;
+				curNode.next = newNode;
+				result = true;
+				break;
+			}
+			curNode = curNode.next;
+		}
+		System.out.println(newNode.previous.data+" "+newNode.data+" "+newNode.next.data);
+		return result;
+	}
+	/////Assuming list is not empty//////////
+	public boolean addAfter(int after,int data){
+		Node curNode = first;
+		while(curNode.data !=after){
+			curNode = curNode.next;
+			if(curNode == null)
+				return false;
+		}
+		Node newNode = new Node();
+		newNode.data = data;
+		if(curNode == last){
+			curNode.next = null;
+			last = newNode;
+		}else{
+			newNode.next = curNode.next;
+			curNode.next.previous = newNode;
+			
+		}
+		newNode.previous = curNode;
+		curNode.next = newNode;
+		return true;
+	}
+	
+	public int removeKey(int key){
+		int temp =-1;
+		Node curNode = first;
+		while(curNode !=null){
+			if(curNode.data == key){
+				temp = curNode.data;
+				if(curNode == last){
+					curNode.previous.next = null;
+					last = curNode.previous;
+				}else if(curNode == first){
+					first = curNode.next;
+				}else{
+					curNode.next.previous = curNode.previous;
+					curNode.previous.next = curNode.next;
+				}
+				break;
+			}
+			curNode = curNode.next;
+		}
+		return temp;
 	}
 	public void view(){
 		Node curNode = first;
